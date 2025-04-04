e440f1db8c4d11c31dff1d12b69ca8821ccb3540
Ramondo Da flaency <123100916+DeadFleanchy@users.noreply.github.com>
2025-04-04 17:30:00 +0200

    Update App.js
    The change of the Learning part.

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React not for Me ,we are Using Django.
         </a>
       </header>
     </div>
